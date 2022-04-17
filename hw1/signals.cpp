75fc05c8fbb59e28d24004f9d397b7922e920c1e
Alon Hartman <alon.hartman@campus.technion.ac.il>
2022-04-17 13:40:44 +0000

    added signals handling

--- hw1/signals.cpp
+++ hw1/signals.cpp
@@ -7,10 +7,33 @@ using namespace std;
 
 void ctrlZHandler(int sig_num) {
 	// TODO: Add your implementation
+  std::cout << "smash: got ctrl-Z" << endl;
+  SmallShell& shell = SmallShell::getInstance();
+  if(shell.foreground_cmd != nullptr) {
+    int success = kill(shell.foreground_cmd->child_pid, sig_num);
+    std::cout<< "sig number: "<< sig_num <<endl;
+    if(success == -1){
+      perror("smash error: kill failed\n");
+      return;
+    }
+    shell.moveToBackground(shell.foreground_cmd, true);
+    shell.foreground_cmd = nullptr;
+  }
 }
 
 void ctrlCHandler(int sig_num) {
   // TODO: Add your implementation
+  std::cout << "smash: got ctrl-C" << endl;
+  SmallShell& shell = SmallShell::getInstance();
+  if(shell.foreground_cmd != nullptr) {
+    int success = kill(shell.foreground_cmd->child_pid, sig_num);
+    std::cout<< "sig number: "<< sig_num <<endl;
+    if(success == -1){
+      perror("smash error: kill failed\n");
+      return;
+    }
+    shell.foreground_cmd = nullptr;
+  }
 }
 
 void alarmHandler(int sig_num) {
